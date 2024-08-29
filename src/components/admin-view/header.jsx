2afcded65cd7ca2ef8e1fe806a77496a2fde5843
RashedujjamanNoor <noorrk042@gmail.com>
2024-08-29 23:18:05 +0600

    create admin and shopping subroutes and 404 page route

--- src/components/admin-view/header.jsx
+++ src/components/admin-view/header.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const AdminHeader = () => {
   return <div>AdminHeader</div>;
 };
--- src/components/shopping-view/header.jsx
+++ src/components/shopping-view/header.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ShoppingHeader = () => {
   return <div>ShoppingHeader</div>;
 };
